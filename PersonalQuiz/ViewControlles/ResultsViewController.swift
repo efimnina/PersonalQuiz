93bda0030ceb1dd8d90181be2ab4099e9a904bfa
Nina <ciz0@bk.ru>
2020-06-04 14:54:32 +0300

    Готово

---
+++ PersonalQuiz/ViewControlles/ResultsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ResultsViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 01.06.2020.
+//  Copyright © 2020 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    var dog = AnimalType.dog
+    var cat = AnimalType.cat
+    var rabbit = AnimalType.rabbit
+    var turtle = AnimalType.turtle
+    
+    // 1. Передать массив с ответами на этот экран
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результаты на экране
+    // 4. Избавится от кнопки back
+
+   
+    @IBOutlet var emojiLabel: UILabel!
+    @IBOutlet var aboutPetLabel: UILabel!
+    var answers: [Answer]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        let resultAnimal = calculat()
+        emojiLabel.text = "Вы - " + String(resultAnimal.rawValue)
+        aboutPetLabel.text = resultAnimal.definition
+        
+    }
+
+    
+func calculat() -> AnimalType {
+    
+    var counts: [AnimalType : Int] = [:]
+    
+    for answer in answers {
+        counts[answer.type] = (counts[answer.type] ?? 0) + 1
+    }
+    
+    let result = counts.sorted(by: { $0.value > $1.value})
+    return result.first!.key
+}
+    
+}
